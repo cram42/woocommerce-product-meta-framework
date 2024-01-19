f3d35b20031f7108bcfc62369a1b8e4fd8a10ba6
Cram42 <cram42@users.noreply.github.com>
2024-01-19 10:16:38 +0800

    Load DatabaseList by FQ class name

--- src/DBFields/DBSelectField.php
+++ src/DBFields/DBSelectField.php
@@ -4,11 +4,23 @@ namespace WCProductMetaFramework;
 
 abstract class DBSelectField extends SelectField
 {
-    protected object $db_list;
+    protected string $dblist_class = '';
+    protected mixed $dblist = null;
+
+    protected function getDBList()
+    {
+        if ($this->dblist == null) {
+            if (empty($this->dblist_class)) {
+                throw new \Exception("dblist_class not provided.");
+            }
+            $this->dblist = $this->dblist_class::getInstance();
+        }
+        return $this->dblist;
+    }
 
     protected function getOptions() {
         if (count($this->options) == 0) {
-            $this->options = $this->db_list->getItems();
+            $this->options = $this->getDBList()->getItems();
         }
         return $this->options;
     }
