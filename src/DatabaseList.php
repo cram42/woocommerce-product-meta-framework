794a242f5cc57490d5741c5dc5c262fef44ecf5c
Cram42 <cram42@users.noreply.github.com>
2024-01-19 16:34:00 +0800

    Extend DBTable

--- src/DatabaseList.php
+++ src/DatabaseList.php
@@ -4,63 +4,29 @@ namespace WCProductMetaFramework;
 
 use WPPluginFramework as WPF;
 
-abstract class DatabaseList extends WPF\Singleton implements
-    WPF\IOnPluginLoad,
-    WPF\IOnPluginEnable,
-    WPF\IOnPluginDisable,
-    WPF\IOnPluginUninstall
+abstract class DatabaseList extends WPF\DBTable
 {
-    protected const VALID_ORDER_BY = array('id', 'value', 'label');
-
-    protected string $id;
-    protected string $table_name;
-    protected $default_items = array();
-
     protected $items;
 
-    protected bool $wipe_on_disable = false;
-    protected bool $wipe_on_uninstall = true;
+    protected const VALID_ORDER_BY = array('id', 'value', 'label');
+    
+    protected array $column_definitions = array(
+        'value varchar(100) NOT NULL',
+        'label varchar(100) NOT NULL',
+    );
 
-    protected function createTable()
+    protected array $default_records = array();
+
+    public function getId(): string
     {
-        global $wpdb;
-        error_log("DatabaseList({$this->getId()})::createTable()");
-
-        $query = sprintf("
-            CREATE TABLE %s (
-                id int NOT NULL AUTO_INCREMENT,
-                value varchar(100) NOT NULL,
-                label varchar(100) NOT NULL,
-                PRIMARY KEY (id)
-            ) %s;",
-            $this->getTableName(),
-            $wpdb->get_charset_collate()
-        );
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($query);
-    }
-
-    protected function deleteTable()
-    {
-        global $wpdb;
-        error_log("DatabaseList({$this->getId()})::deleteTable()");
-
-        $query = sprintf("DROP TABLE IF EXISTS %s;", $this->getTableName());
-        $wpdb->query($query);
-    }
-
-    protected function populateTable()
-    {
-        global $wpdb;
-        error_log("DatabaseList({$this->getId()})::populateTable()");
-        
-        foreach ($this->default_items as $item) {
-            $wpdb->replace($this->getTableName(), $item);
+        if (empty($this->id)) {
+            $class_name = get_class($this);
+            $class_name = preg_replace('/List$/', '', $class_name);
+            $class_name = strtolower(str_replace('\\', '_', $class_name));
+            $this->id = $class_name;
         }
+        return $this->id;
     }
-
-    #region Public Methods
 
     public function getItems($order_by = null, $order_desc = false)
     {
@@ -71,10 +37,7 @@ abstract class DatabaseList extends WPF\Singleton implements
             : static::VALID_ORDER_BY[0];
 
         if ($this->items == null) {
-            global $wpdb;
-            $query = sprintf("SELECT value, label FROM %s ORDER BY %s %s;", $this->getTableName(), $order_by, ($order_desc) ? 'DESC' : '');
-            $results = $wpdb->get_results($query);
-            
+            $results = $this->readAll();
             $this->items = array();
             foreach ($results as $result) {
                 $this->items[$result->value] = $result->label;
@@ -82,54 +45,4 @@ abstract class DatabaseList extends WPF\Singleton implements
         }
         return $this->items;
     }
-
-    public function getId() : string
-    {
-        if (empty($this->id)) {
-            $class_name = get_class($this);
-            $class_name = preg_replace('/Field$/', '', $class_name);
-            $class_name = strtolower(str_replace('\\', '_', $class_name));
-            $this->id = $class_name;
-        }
-        return $this->id;
-    }
-
-    public function getTableName() : string
-    {
-        if (empty($this->table_name)) {
-            global $wpdb;
-            $table_name = $wpdb->prefix . $this->getId();
-            $this->table_name = $table_name;
-        }
-        return $this->table_name;
-    }
-
-    public function OnPluginLoad() : void
-    {
-        error_log("DatabaseList({$this->getId()})::OnPluginLoad()");
-    }
-
-    public function OnPluginEnable() : void
-    {
-        error_log("DatabaseList({$this->getId()})::OnPluginEnable()");
-        $this->createTable();
-        $this->populateTable();
-    }
-
-    public function OnPluginDisable() : void
-    {
-        error_log("DatabaseList({$this->getId()})::OnPluginDisable()");
-        if ($this->wipe_on_disable) {
-            $this->deleteTable();
-        }
-    }
-
-    public function OnPluginUninstall() : void
-    {
-        error_log("DatabaseList({$this->getId()})::OnPluginUninstall()");
-        if ($this->wipe_on_uninstall) {
-            $this->deleteTable();
-        }
-    }
-    #endregion
 }
