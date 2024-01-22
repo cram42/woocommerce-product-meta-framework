fe9f1e2fee45a4e2f420d12ea775c6f3e9372c5a
Cram42 <cram42@users.noreply.github.com>
2024-01-22 08:25:50 +0800

    Implement new method

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ if (!class_exists('WPPluginFramework\ClassFinder')) {
     throw new \Exception("Must load WordPress Plugin Framework first.");
 }
 
-\WPPluginFramework\ClassFinder::register();
-$wcppf_class_finder_handle = \WPPluginFramework\ClassFinder::getHandle();
-$wcppf_class_finder_handle->addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src', true);
-$wcppf_class_finder_handle->addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src/StaticFields', true);
-$wcppf_class_finder_handle->addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src/DBFields', true);
+\WPPluginFramework\ClassFinder::addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src', true);
+\WPPluginFramework\ClassFinder::addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src/StaticFields', true);
+\WPPluginFramework\ClassFinder::addNamespacePath('WCProductMetaFramework', dirname(__FILE__) . '/src/DBFields', true);
